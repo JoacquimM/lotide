3b6b9c6a80314cbc7e9e5b6f3b041212bc8382f5
Joacquim Mukadi <joacquimm@gmail.com>
2021-03-04 00:48:02 +0000

    new file countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,78 @@
+// // FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  const errorMessage = `❌ ❌ ❌ Assertion Failed: ${actual} !== ${expected}`;
+  const successMessage = `✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`
+
+  // console.assert(`${actual}` === `${expected}`, {successMessage});
+  // console.assert(`${actual}` !== `${expected}`, {errorMessage});
+  if (actual === expected) {console.log(successMessage)
+  }else{console.log(errorMessage)
+  }
+
+
+};
+
+// // TEST CODE
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+
+
+/*
+The bellow function should take in a sentence (as a string) and
+return a count of each of the letters in teh sentence.
+ */
+
+
+// const countLetters = (str) => {
+//   let stringCopy = str.replace(/\W/g, '');
+//   let report = {};
+//   let stringCatcher = '';
+//   for (let letter in stringCopy) {
+//     stringCopy.match(/[a-zA-Z]/g) ? letterIncrementer() /*console.log(stringCopy[letter])*/ : console.log('no match');
+//     function letterIncrementer() {
+//       // TODO: write function to increment letters 
+//       report[`${stringCopy[letter]}`] = 1; // creating key value pair
+//       stringCatcher.match(stringCopy[letter]) /* checking if letter exists*/? report[`${stringCopy[letter]}`] +=1 : stringCatcher += stringCopy[letter]//creating the string;
+
+//       //stringCatcher += stringCopy[letter];
+
+//     }
+//   }
+
+//   console.log( stringCatcher,report/*str, stringCopy*/);
+
+
+// }
+
+const countLetters = (str) => {
+  let stringCopy = str.replace(/\W/g, '');
+  let report = {};
+  let stringCatcher = '';
+  for (let letter in stringCopy) {
+    stringCopy.match(/[a-zA-Z]/g) ? letterIncrementer() : console.log('no match');
+    function letterIncrementer() {
+      report[`${stringCopy[letter]}`] = 0; // creating key value pair
+      stringCatcher += stringCopy[letter];
+      // console.log(stringCopy[letter]);
+    }
+  }
+  
+  for( let i in stringCatcher){
+    //console.log(stringCatcher[i]);
+     report[`${stringCatcher[i]}`] +=1 ;
+  }
+
+  console.log( /*stringCatcher,*/report/*str, stringCopy*/);
+}
+
+
+
+
+
+//countLetters('Joacquim Mukadi')
+countLetters('lighthouse in the house')
+
+const string = 'lighthouse in the house'
+countLetters(string);
+assertEqual(string,'lighthouse in the house')
+
