93fef68d91a6212b482276a628c8917fa0109c43
Joacquim Mukadi <joacquimm@gmail.com>
2021-03-08 13:54:00 +0000

    exported head.js test code to headTest.js file to seperate test code from function

--- head.js
+++ head.js
@@ -1,28 +1,34 @@
 
 
-
+const assertEqual = require('./assertEqual'); // <---importing function from main file. 
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  const errorMessage = `❌ ❌ ❌ Assertion Failed: ${actual} !== ${expected}`;
-  const successMessage = `✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`
+//asserEqual function bellow commented due to importing function from original file.
 
-  if (actual === expected) {console.log(successMessage)
-  }else{console.log(errorMessage)
-  }
+// const assertEqual = function(actual, expected) {
+//   const errorMessage = `❌ ❌ ❌ Assertion Failed: ${actual} !== ${expected}`;
+//   const successMessage = `✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`
+
+//   if (actual === expected) {console.log(successMessage)
+//   }else{console.log(errorMessage)
+//   }
 
 
-};
+// };
 
 const head = (arr) => {
   return arr[0];
 
 };
 
-
+// Tests for heaed
 // console.log(head([5,6,7]));
 // console.log(head(["Hello", "Lighthouse", "Labs"]));
 // console.log(head([]));
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined)
+// Test for assertEqual and head  below (do not change)
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([]), undefined)
+
+
+module.exports = head;
