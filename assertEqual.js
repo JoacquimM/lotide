a2b440772b45e271ee02e9d6be719a1bc0780fc9
Joacquim Mukadi <joacquimm@gmail.com>
2021-03-08 13:56:27 +0000

    exported assertEqual functiona and seperated test code of assertEqual function to assertEqaultest.js file in order to seperate test code from function to not recive test outputs from function where the function is being required

--- assertEqual.js
+++ assertEqual.js
@@ -11,7 +11,15 @@ const assertEqual = function(actual, expected) {
 
 
 };
+/* Test code commented out due to transport in to assertEqualTest.js 
+  ( did this b/c it was causing extra test cases to run from assertEqual.js to head.js 
+   when we exported the function to head.js.  Moved assertEqual tets code in to a assertEqualTest file in 
+   order to seperate it from the main function file.head.js so it wouldnt export the test codes wherever 
+   the function was being required).
+*/
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+// // TEST CODE
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+
+module.exports = assertEqual; // <---- exporting the function assertEqual() to wherever it wants to be usen by requireing it. 
